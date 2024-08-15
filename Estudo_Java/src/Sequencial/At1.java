68089755a871f1e87d833093c67f1105c97d2d88
Antonio Neto <132214964+AntonioJsNeto@users.noreply.github.com>
2024-08-15 08:38:37 -0300

    Inicio dos estudos.

---
+++ Estudo_Java/src/Sequencial/At1.java
@@ -0,0 +1,18 @@
+package Sequencial;
+
+import java.util.Scanner;
+
+public class At1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x, y;
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        int soma = x + y;
+
+        System.out.println("SOMA = " + soma);
+    }
+}
