1d701f255023406a25589b9b571d7e57abfc9f2a
Antonio Neto <132214964+AntonioJsNeto@users.noreply.github.com>
2024-08-16 18:53:30 -0300

    Finalizações

---
+++ Estudo_Java/src/Sequencial/At4.java
@@ -0,0 +1,23 @@
+package Sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class At4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number, horasTrabalhadas;
+        double salary, valorPorHora;
+
+        number = sc.nextInt();
+        valorPorHora = sc.nextDouble();
+        horasTrabalhadas = sc.nextInt();
+
+        salary = horasTrabalhadas * valorPorHora;
+
+        System.out.println("NUMBER = " + number);
+        Locale.setDefault(Locale.US);
+        System.out.printf("SALARY =  U$ %.2f %n", salary);
+    }
+}
---
+++ Estudo_Java/src/Sequencial/At5.java
@@ -0,0 +1,28 @@
+package Sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class At5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cod1, cod2, qtd1, qtd2;
+        double preco1, preco2;
+
+        System.out.println("Informe sobre a peça um: ");
+        cod1 = sc.nextInt();
+        qtd1 = sc.nextInt();
+        preco1 = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Agora a peça dois: ");
+        cod2 = sc.nextInt();
+        qtd2 = sc.nextInt();
+        preco2 = sc.nextDouble();
+
+        double total = (qtd1 * preco1) + (qtd2 * preco2);
+        Locale.setDefault(Locale.US);
+        System.out.printf("VALOR A PAGAR: R$ %.2f %n", total);
+    }
+}
