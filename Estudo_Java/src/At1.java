200460d1c32c9b1b3ac8171c03ccb415e7ca46b2
Antonio Neto <132214964+AntonioJsNeto@users.noreply.github.com>
2024-09-05 18:55:26 -0300

    Add files via upload

---
+++ Estudo_Java/src/At1.java
@@ -0,0 +1,20 @@
+package Repeticao;
+
+import java.util.Scanner;
+
+public class At1 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+        System.out.println("INFORME A SENHA: ");
+    int senha = sc.nextInt();
+
+    while(senha != 2002){
+        if(senha != 2002){
+            System.out.println("SENHA INVÁLIDA!");
+        }
+        senha = sc.nextInt();
+    }
+    System.out.println("ACESSO PERMITIDO!");
+    }
+}
